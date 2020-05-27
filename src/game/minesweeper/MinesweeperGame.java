ba221632a2aeb93ab09d6195a9ad0bc21904b6be
EugeneAhramenko <akhramenko92@mail.ru>
2020-05-27 18:29:47 +0300

    first commit minesweeper

---
+++ src/game/minesweeper/MinesweeperGame.java
@@ -0,0 +1,9 @@
+package game.minesweeper;
+
+public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    public void initialize(){
+        this.setScreenSize(SIDE, SIDE);
+
+    }
+}
